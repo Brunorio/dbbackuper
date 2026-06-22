4ade660943f66120a6f7528b25a538642a20826e
Bruno Santos <gregoquiral@gmail.com>
2026-06-22 15:58:17 -0300

    remove ssl config mode

--- src/backup-generator/providers/Mydumper.ts
+++ src/backup-generator/providers/Mydumper.ts
@@ -8,7 +8,7 @@ export default class MydumperProviderBackupGenerator extends BackupGenerator {
     const filename = this.generateBackupName();
     const pathfolder = `${this.pathFolder}/${filename}`;
 
-    execSync(`mydumper --host ${this.credentials.host} --user ${this.credentials.user} --password '${this.credentials.password}' --port ${this.credentials.port} --database ${this.credentials.database} --outputdir ${pathfolder} --compress --triggers --events --routines --verbose 3 --ssl-mode DISABLED`);
+    execSync(`mydumper --host ${this.credentials.host} --user ${this.credentials.user} --password '${this.credentials.password}' --port ${this.credentials.port} --database ${this.credentials.database} --outputdir ${pathfolder} --compress --triggers --events --routines --verbose 3`);
 
     await CompressFolder.compress(this.pathFolder, filename);
   
